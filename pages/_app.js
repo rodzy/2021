4f4da6d390edccd1398123cff9ce447182a7bf5e
rodzy <49137701+rodzy@users.noreply.github.com>
2020-10-01 17:21:51 -0600

    Added motion animations on page changed ✨

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,25 @@
 import { ModeProvider } from "../context/ModeContext";
+import { motion } from "framer-motion";
 import "../styles/global.css";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps, router }) {
     return (
         <ModeProvider>
-            <Component {...pageProps} />
+            <motion.div
+                key={router.route}
+                initial="pageInitial"
+                animate="pageAnimate"
+                variants={{
+                    pageInitial: {
+                        opacity: 0,
+                    },
+                    pageAnimate: {
+                        opacity: 1,
+                    },
+                }}
+            >
+                <Component {...pageProps} />
+            </motion.div>
         </ModeProvider>
     );
 }
