e42da1426f0cbf9f76736e36cd99a128054213e1
rodzy <49137701+rodzy@users.noreply.github.com>
2020-10-01 20:42:36 -0600

    Animations before entering the re-render done ✅

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,31 @@
 import { ModeProvider } from "../context/ModeContext";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import "../styles/global.css";
 
 export default function App({ Component, pageProps, router }) {
     return (
         <ModeProvider>
-            <motion.div
-                key={router.route}
-                initial="pageInitial"
-                animate="pageAnimate"
-                variants={{
-                    pageInitial: {
-                        opacity: 0,
-                    },
-                    pageAnimate: {
-                        opacity: 1,
-                    },
-                }}
-            >
-                <Component {...pageProps} />
-            </motion.div>
+            <AnimatePresence initial={false} exitBeforeEnter={true}>
+                <motion.div
+                    key={router.route}
+                    initial="pageInitial"
+                    animate="pageAnimate"
+                    exit="pageExit"
+                    variants={{
+                        pageInitial: {
+                            opacity: 0,
+                        },
+                        pageAnimate: {
+                            opacity: 1,
+                        },
+                        pageExit: {
+                            opacity: 0,
+                        },
+                    }}
+                >
+                    <Component {...pageProps} />
+                </motion.div>
+            </AnimatePresence>
         </ModeProvider>
     );
 }
