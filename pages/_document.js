4e8091874df3fe2bcbdf68c18737dec0ac8bb7ab
rodzy <49137701+rodzy@users.noreply.github.com>
2020-10-01 18:21:31 -0600

    Improvements on the Open Graph SEO

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { siteTitle } from "../components/layout";
+import { siteTitle, title } from "../components/layout";
 class MyDocument extends Document {
     render() {
         return (
@@ -13,12 +13,30 @@ class MyDocument extends Document {
                         name="viewport"
                         content="width=device-width, initial-scale=1.0"
                     />
+                    {/* Twitter tags */}
+                    <meta name="twitter:card" content="summary" />
+                    <meta name="twitter:title" content={siteTitle} />
+                    <meta
+                        name="twitter:description"
+                        content={siteTitle + `, ` + title}
+                    />
+                    <meta name="twitter:creator" content="@rodzyrm" />
+                    <meta
+                        name="twitter:image"
+                        content="https://avatars1.githubusercontent.com/u/49137701?s=400&u=35e7614cc3215c045b58b4e352bd0616b9d795fd&v=4"
+                    />
+                    {/*Open graph tags */}
+                    <meta name="og:title" content={siteTitle} />
+                    <meta name="og:url" content="https://rodzy.vercel.app" />
+                    <meta
+                        name="og:description"
+                        content={siteTitle + `, ` + title}
+                    />
+                    <meta name="og:site_name" content={siteTitle} />
                     <meta
                         property="og:image"
-                        content={`https://avatars1.githubusercontent.com/u/49137701?s=400&u=35e7614cc3215c045b58b4e352bd0616b9d795fd&v=4`}
+                        content="https://avatars1.githubusercontent.com/u/49137701?s=400&u=35e7614cc3215c045b58b4e352bd0616b9d795fd&v=4"
                     />
-                    <meta name="og:title" content={siteTitle} />
-                    <meta name="twitter:card" content="summary_large_image" />
                     <link rel="icon" href="/favicon.ico" />
                 </Head>
                 <body>
